19a82f75896e38cad2aada3455fa8e127ff362d1
Michael Medis <TheMedisGroup@gmail.com>
2016-02-19 22:22:54 -0500

    Checkpoint 23, completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,7 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
-
+var animatePoints = function(points) {
     var revealPoint = function(i) {
       points[i].style.opacity = 1;
       points[i].style.transform = "scaleX(1) translateY(0)";
@@ -9,7 +8,24 @@ var animatePoints = function() {
       points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 
-    for (var x = 0; x < points.length; x++) {
-      revealPoint(x);
+    for (var i = 0; i < points.length; i++) {
+      revealPoint(i);
     }
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener("scroll", function(event) {
+    var msg = "Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels";
+    console.log(msg);
+   if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+     animatePoints(pointsArray);
+    }
+  });
+}
