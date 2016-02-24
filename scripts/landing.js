0116d78b28ddb1e4d0b3c22a22e2cc20b6fdbb58
Michael Medis <TheMedisGroup@gmail.com>
2016-02-24 10:57:13 -0500

    Fixed landing.js, completed checkpoint

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,29 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
-    var revealPoint = function(i) {
-      points[i].style.opacity = 1;
-      points[i].style.transform = "scaleX(1) translateY(0)";
-      points[i].style.msTransform = "scaleX(1) translateY(0)";
-      points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
 
     for (var i = 0; i < points.length; i++) {
-      revealPoint(i);
+        revealPoint(i);
     }
 };
 
 window.onload = function() {
-  if (window.innerHeight > 950) {
-    animatePoints(pointsArray);
-  }
-
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-
-  window.addEventListener("scroll", function(event) {
-    var msg = "Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels";
-    console.log(msg);
-   if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-     animatePoints(pointsArray);
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
     }
-  });
+
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener("scroll", function(event) {
+        if (document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
 }
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+function forEach(a, cb) {
+  for (var i = 0; i < a.length; i++) {
+    cb(a[i]);
+  }
+}
