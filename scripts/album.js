3d26bbb50a4dcce0508f2fb442a78b499db3631f
Michael Medis <TheMedisGroup@gmail.com>
2016-02-29 01:59:10 -0500

    Assignment 27 completed

--- scripts/album.js
+++ scripts/album.js
@@ -1,4 +1,5 @@
-
+// "use strict"; an error is called
+//on the i in line 60 when using the "use strict" method. Can why talk about how we could work around this.
 
 var albumPicasso = {
 	name: 'The Colors',
@@ -60,82 +61,104 @@ var setCurrentAlbum = function(album) {
 		albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
 	}
 };
+//use my code from the checkpoint and refactored and rearranged to find solution.
+//would love to ralk on how I could make my while loop work with alerts.
+var babyChild = document.getElementsByClassName('album-view-title')[0];
+var classLessBaby = document.querySelector('html');
 
 var findParentByClassName = function(element, targetClass) {
 	var currentParent = element.parentElement;
-	while (currentParent.className != targetClass) {
-		currentParent = currentParent.parentElement;
-	}
-	return currentParent;
-};
 
-var getSongItem = function(element) {
-	switch (element.className) {
-		case 'album-song-button':
-		case 'ion-play':
-		case 'ion-pause':
-		return findParentByClassName(element, 'song-item-number');
-		case 'album-view-song-item':
-		return element.querySelector('.song-item-number');
-		case 'song-item-title':
-		case 'song-item-duration':
-		return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-		case 'song-item-number':
-		return element;
-		default:
-		return;
-	}
-};
-
-var clickHandler = function(targetElement) {
-	var songItem = getSongItem(targetElement);
-
-	if (currentlyPlayingSong === null) {
-		songItem.innerHTML = pauseButtonTemplate;
-		currentlyPlayingSong = songItem.getAttribute('data-song-number');
-	} else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-		songItem.innerHTML = playButtonTemplate;
-		currentlyPlayingSong = null;
-	} else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-		var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-		currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-		songItem.innerHTML = pauseButtonTemplate;
-		currentlyPlayingSong = songItem.getAttribute('data-song-number');
-	}
-};
-
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
-
-var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-
-var currentlyPlayingSong = null;
-
-window.onload = function() {
-	setCurrentAlbum(albumPicasso);
-
-	songListContainer.addEventListener('mouseover', function(event) {
-		if (event.target.parentElement.className === 'album-view-song-item') {
-			var songItem = getSongItem(event.target);
-
-			if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-				songItem.innerHTML = playButtonTemplate;
-			}
+	if (currentParent) {
+		while (currentParent.className && currentParent.className != targetClass) {
+			currentParent = currentParent.parentElement;
 		}
-	});
 
-	for (i = 0; i < songRows.length; i++) {
-		songRows[i].addEventListener('mouseleave', function(event) {
-			var songItem = getSongItem(event.target);
-			var songItemNumber = songItem.getAttribute('data-song-number');
-
-			if (songItemNumber !== currentlyPlayingSong) {
-				songItem.innerHTML = songItemNumber;
-			}
-		});
-		songRows[i].addEventListener('click', function(event) {
-			clickHandler(event.target);
-		});
+		if (currentParent.className == targetClass) {
+			return currentParent;
+		} else {
+			alert("Has a class just no parent.");
+		}
+	} else {
+		alert("No parent. NOOOO!");
 	}
 };
+//can use the provide variables to get the intended result.
+findParentByClassName(classLessBaby, "not gonna work" );
+
+//my favorite function so far found based off code from http://goo.gl/NrUzSs
+//my function from stackover http://stackoverflow.com/questions/22119673/find-the-closest-ancestor-element-that-has-a-specific-class
+var findParentByClassName = function(element, searchClass){
+	console.log('findParentByClassName', element);
+	while((element = element.parentElement) && element.classList.contains(searchClass)){return element;}
+};
+	var getSongItem = function(element) {
+		switch (element.className) {
+			case 'album-song-button':
+			case 'ion-play':
+			case 'ion-pause':
+			return findParentByClassName(element, 'song-item-number');
+			case 'album-view-song-item':
+			return element.querySelector('.song-item-number');
+			case 'song-item-title':
+			case 'song-item-duration':
+			return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+			case 'song-item-number':
+			return element;
+			default:
+			return;
+		}
+	};
+
+	var clickHandler = function(targetElement) {
+		var songItem = getSongItem(targetElement);
+
+		if (currentlyPlayingSong === null) {
+			songItem.innerHTML = pauseButtonTemplate; // getting Uncaught TypeError Cannot set prop 'innerHTML' of undefined
+			currentlyPlayingSong = songItem.getAttribute('data-song-number');
+		} else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+			songItem.innerHTML = playButtonTemplate;
+			currentlyPlayingSong = null;
+		} else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+			var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+			currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+			songItem.innerHTML = pauseButtonTemplate;
+			currentlyPlayingSong = songItem.getAttribute('data-song-number');
+		}
+	};
+
+	var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+	var songRows = document.getElementsByClassName('album-view-song-item');
+
+	var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+	var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+	var currentlyPlayingSong = null;
+
+	window.onload = function() {
+		setCurrentAlbum(albumPicasso);
+
+		songListContainer.addEventListener('mouseover', function(event) {
+			if (event.target.parentElement.className === 'album-view-song-item') {
+				var songItem = getSongItem(event.target);
+
+				if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+					songItem.innerHTML = playButtonTemplate;
+				}
+			}
+		});
+
+		for (i = 0; i < songRows.length; i++) {
+			songRows[i].addEventListener('mouseleave', function(event) {
+				var songItem = getSongItem(event.target);
+				var songItemNumber = songItem.getAttribute('data-song-number');
+
+				if (songItemNumber !== currentlyPlayingSong) {
+					songItem.innerHTML = songItemNumber;
+				}
+			});
+			songRows[i].addEventListener('click', function(event) {
+				clickHandler(event.target);
+			});
+		}
+	};
