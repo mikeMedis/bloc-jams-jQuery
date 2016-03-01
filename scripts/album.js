b0cf61e47ec2f44a6376c37416e119bc3d2b7ad7
Michael Medis <TheMedisGroup@gmail.com>
2016-03-01 13:40:24 -0500

    Checkpoint 28, finished...still needs cleaned up

--- scripts/album.js
+++ scripts/album.js
@@ -74,16 +74,16 @@ var getSongItem = function(element) {
 		case 'album-song-button':
 		case 'ion-play':
 		case 'ion-pause':
-		return findParentByClassName(element, 'song-item-number');
+			return findParentByClassName(element, 'song-item-number');
 		case 'album-view-song-item':
-		return element.querySelector('.song-item-number');
+			return element.querySelector('.song-item-number');
 		case 'song-item-title':
 		case 'song-item-duration':
-		return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+			return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
 		case 'song-item-number':
-		return element;
+			return element;
 		default:
-		return;
+			return;
 	}
 };
 
